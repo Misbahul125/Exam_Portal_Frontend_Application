7b9274f910d924d3cdeafee9e7047007622300e7
Misbahul Haque <misbahulhaque2001@gmail.com>
2022-02-09 23:18:49 +0530

    Data Binding for Signup added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -29,6 +30,7 @@ import { LoginComponent } from './pages/login/login.component';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -9,7 +9,53 @@ export class SignupComponent implements OnInit {
 
   constructor() { }
 
+  public user= {
+    userName: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    emailId: '',
+    contactNumber: '',
+  };
+
   ngOnInit(): void {
+  }
+
+  userSignup() {
+
+    if(this.user.userName == '' || this.user.userName == null) {
+      alert('Username cannot be empty!');
+      return;
+    }
+
+    if(this.user.password == '' || this.user.password == null) {
+      alert('Password cannot be empty!');
+      return;
+    }
+
+    if(this.user.firstName == '' || this.user.firstName == null) {
+      alert('First Name cannot be empty!');
+      return;
+    }
+
+    if(this.user.lastName == '' || this.user.lastName == null) {
+      alert('Lat Name cannot be empty!');
+      return;
+    }
+
+    if(this.user.emailId == '' || this.user.emailId == null) {
+      alert('Email cannot be empty!');
+      return;
+    }
+
+    if(this.user.contactNumber == '' || this.user.contactNumber == null) {
+      alert('Contact Number cannot be empty!');
+      return;
+    }
+
+    console.log(this.user);
+
+    alert('submit');
   }
 
 }
