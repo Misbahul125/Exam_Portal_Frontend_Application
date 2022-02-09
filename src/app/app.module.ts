84f4899994f6e649e2382cab366fd0f5c17cf946
Misbahul Haque <misbahulhaque2001@gmail.com>
2022-02-10 05:12:47 +0530

    SignUp UI updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -25,12 +27,14 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import Swal from 'sweetalert2'
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private matSnackBar: MatSnackBar,
+    ) { }
 
   public user= {
     userName: '',
@@ -24,38 +30,107 @@ export class SignupComponent implements OnInit {
   userSignup() {
 
     if(this.user.userName == '' || this.user.userName == null) {
-      alert('Username cannot be empty!');
+      //alert('Username cannot be empty!');
+      this.matSnackBar.open('Username is required !!' , 'OK' , {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      });
       return;
+    }
+    else {
+      //check username is unique or not
     }
 
     if(this.user.password == '' || this.user.password == null) {
-      alert('Password cannot be empty!');
+      //alert('Password cannot be empty!');
+      this.matSnackBar.open('Password cannot be empty !!' , 'OK' , {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      });
       return;
+    }
+    else {
+      //check password constraints
+      //if((this.user.password.))
     }
 
     if(this.user.firstName == '' || this.user.firstName == null) {
-      alert('First Name cannot be empty!');
+      //alert('First Name cannot be empty!');
+      this.matSnackBar.open('First Name is required !!' , 'OK' , {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      });
       return;
     }
 
     if(this.user.lastName == '' || this.user.lastName == null) {
-      alert('Lat Name cannot be empty!');
+      //alert('Lat Name cannot be empty!');
+      this.matSnackBar.open('Last Name is required !!' , 'OK' , {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      });
       return;
     }
 
     if(this.user.emailId == '' || this.user.emailId == null) {
-      alert('Email cannot be empty!');
+      //alert('Email cannot be empty!');
+      this.matSnackBar.open('Email is required !!' , 'OK' , {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      });
       return;
+    }
+    else {
+      //check if email is valid or not
+
     }
 
     if(this.user.contactNumber == '' || this.user.contactNumber == null) {
-      alert('Contact Number cannot be empty!');
+      //alert('Contact Number cannot be empty!');
+      this.matSnackBar.open('Contact Number is required !!' , 'OK' , {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      });
       return;
+    }
+    else {
+      //check if contact number doesn't contain 'e'
     }
 
     console.log(this.user);
+    
 
-    alert('submit');
+    //signUp() from user_service
+    this.userService.createUser(this.user).subscribe(
+      (data: any) => {
+        console.log(data);
+        //alert('SignUp successful');
+        // this.matSnackBar.open('SignUp successful !!' , 'OK' , {
+        //   duration: 3000,
+        //   verticalPosition: 'bottom',
+        //   horizontalPosition: 'center',
+        // });
+
+        Swal.fire('SignUp Successfull !!' , 'You have registered successfully. Your User ID is : '+data.userId , 'success');
+      },
+      (error) => {
+        console.log(error);
+        //alert('Oops! Something went wrong');
+        // this.matSnackBar.open('Oops! Something went wrong' , 'OK' , {
+        //   duration: 3000,
+        //   verticalPosition: 'bottom',
+        //   horizontalPosition: 'center',
+        // });
+
+        Swal.fire('Error' , 'Oops! Something went wrong. Please try again later.' , 'error');
+      }
+    );
   }
 
 }
