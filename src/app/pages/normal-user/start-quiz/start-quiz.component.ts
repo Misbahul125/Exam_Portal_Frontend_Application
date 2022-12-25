d2adf65b7d7aff59aa142634c461f5d307455ff0
Misbahul Haque <misbahulhaque2001@gmail.com>
2022-12-25 21:53:07 +0530

    Test Marks Calculation added

--- src/app/pages/normal-user/start-quiz/start-quiz.component.ts
+++ src/app/pages/normal-user/start-quiz/start-quiz.component.ts
@@ -14,6 +14,12 @@ export class StartQuizComponent implements OnInit {
   quizId: any;
   questions: any;
 
+  marksObtained = 0;
+  correctAnswers = 0;
+  questionsAttempted = 0;
+
+  isSubmitted = false;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private locationStrategy: LocationStrategy,
@@ -24,7 +30,7 @@ export class StartQuizComponent implements OnInit {
     this.preventBackButton();
     this.quizId = this.activatedRoute.snapshot.params['quizId'];
     //console.log(this.quizId);
-    
+
     this.getQuestionsFOrStartingTest();
 
   }
@@ -40,8 +46,15 @@ export class StartQuizComponent implements OnInit {
 
     this.questionService.getAllQuestionsOfQuiz(this.quizId).subscribe(
       (data) => {
-        console.log(data);
+        //console.log(data);
         this.questions = data;
+
+        this.questions.forEach((q: any) => {
+          q['givenAnswer'] = '';
+        });
+
+        console.log(this.questions);
+
       },
       (error) => {
         console.log(error);
@@ -52,8 +65,44 @@ export class StartQuizComponent implements OnInit {
   }
 
   submitQuiz() {
-    console.log("submit");
-    
+    //console.log("submit");
+
+    Swal.fire({
+      title: 'End Test',
+      text: 'Are you sure?',
+      icon: 'info',
+      showCancelButton: true,
+      //cancelButtonColor: red,
+      focusConfirm: false,
+      confirmButtonColor: 'red',
+      confirmButtonText: 'YES',
+      cancelButtonText: 'NO',
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.isSubmitted = true;
+
+        let singleQuestionMark = this.questions[0].quiz.maxMarks/this.questions.length;
+        
+        this.questions.forEach((question:any) => {
+
+          if(question.givenAnswer == question.answer) {
+            this.correctAnswers++;
+            this.marksObtained += singleQuestionMark;
+          }
+
+          if(question.givenAnswer.trim() != '' && question.givenAnswer != null)
+          this.questionsAttempted++;
+
+        });
+
+        console.log('Correct answer: '+this.correctAnswers);
+        console.log('Marks Obtained: '+this.marksObtained);
+        console.log('Attempted: '+this.questionsAttempted);
+
+      }
+    });
+
   }
 
 }
